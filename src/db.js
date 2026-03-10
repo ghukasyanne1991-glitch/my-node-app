5c4fc0198a7cb90d46f15b79b72ff162dcaf8797
Ghukasyan Anna <ghukasyanna1991@gmail.com>
2026-03-10 17:32:46 +0400

    Fix files

--- src/db.js
+++ src/db.js
@@ -1,5 +1,8 @@
 import mysql from 'mysql2/promise';
 
+import 'dotenv/config';
+//console.log(process.env.DATABASE_URL);
+
 const db = mysql.createPool({
     uri: process.env.DATABASE_URL,
     ssl: {
--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import express from "express";
-
 import db from "./db.js";
+import 'dotenv/config';
 
 const app = express();
 app.use(express.json());
 
 app.get("/health", async (req, res) => {
     try{
-       const [rows] = await db.query ('SELECT 1');
+        //console.log("Trying DB connection...");
+        const [rows] = await db.query ('SELECT 1');
+        //console.log("DB query successful:", rows);
        res.status(200).json({
            status: 'ok',
            db: 'connected'
        });
     } catch (err) {
+        //console.error("DB connection error:", err);
         res.status(500).json({
             status: 'error',
             message: err.message});
